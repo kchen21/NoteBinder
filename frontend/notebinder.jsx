3cc73d9e6f96406be31b0415fe7197f2ef9716c8
Kenneth Chen <kennethchendbgt@gmail.com>
2016-12-04 00:12:03 -0500

    Set up backend and frontend structure

--- frontend/notebinder.jsx
+++ frontend/notebinder.jsx
@@ -1,26 +1,7 @@
-const path = require('path');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-module.exports = {
-  context: __dirname,
-  entry: './frontend/notebinder.jsx',
-  output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  },
-  devtool: 'source-maps'
-};
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to NoteBinder</h1>, root);
+});
