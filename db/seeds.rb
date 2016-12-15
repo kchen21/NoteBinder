bc83eef74bbfed3bd772a5be12e88f0804ff6757
Kenneth Chen <kennethchendbgt@gmail.com>
2016-12-15 10:21:32 -0500

    Edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,15 @@ Notebook.create!(title: "Work", author_id: 1) # id: 2
 Notebook.create!(title: "New York Restaurant Reviews", author_id: 1) # id: 3
 Notebook.create!(title: "Travel Diary", author_id: 1) # id: 4
 Notebook.create!(title: "Favorite Recipes", author_id: 1) # id: 5
-Note.create!(title: "Welcome to Notebinder!", body: "<p>Notebinder is an Evernote-inspired web application built using Ruby on Rails and React/Redux! You can use it to write notes and save them to a database!</p>", notebook_id: 1) # id: 1
-Note.create!(title: "Notes", body: "<p>Your notes are all available here, to the left!</p>", notebook_id: 1) # id: 2
-Note.create!(title: "Notebooks", body: "<p>Click on the Notebooks icon in the sidebar to browse through notebooks and find notes corresponding to each notebook.</p>", notebook_id: 1) # id: 3
-Note.create!(title: "Tags", body: "<p>You can create tags for note-filtering. To view all tags, click on the Tags icon in the sidebar. You create a new tag here, or when creating or updating a note!</p>", notebook_id: 1) # id: 4
-Note.create!(title: "Updating", body: "<p>The contents of a note or notebook can be updated once it is created!</p>", notebook_id: 1) # id: 5
-Note.create!(title: "Deleting", body: "<p>Notes, notebooks, and tags can also be deleted! Be careful when deleting a notebook; it'll delete all the notes in that notebook as well!</p>", notebook_id: 1) # id: 6
+Note.create!(title: "Welcome to Notebinder!", body: "<p>Notebinder is an Evernote-inspired web application built using Ruby on Rails and React/Redux! You can use it to write notes and save them to a database! Click on the 'New Note' (+) icon on the left to create a new note!</p>", notebook_id: 1) # id: 1
+Note.create!(title: "Notes", body: "<p>Your notes are all available in the Notes index, which is available for viewing whenever a note is being created or updated. You can click on the Notes icon in the sidebar to view just the Notes index itself.</p>", notebook_id: 1) # id: 2
+Note.create!(title: "Notebooks", body: "<p>Click on the Notebooks icon in the sidebar to get to the Notebooks index. Once there, feel free to browse through notebooks and find notes corresponding to each notebook. To create a new notebook, click on the New Notebook icon next to 'NOTEBOOKS'. IMPORTANT NOTE: The notebook's description must be less than 256 characters. Keep it short!</p>", notebook_id: 1) # id: 3
+Note.create!(title: "Tags", body: "<p>You can create tags for note-filtering. To view all tags, click on the Tags icon in the sidebar, which opens the Tags index. You can either create a new tag there or when updating a note! Click on a tag from either the Tags index or from a note to access all notes that have that tag.</p>", notebook_id: 1) # id: 4
+Note.create!(title: "Updating", body: "<p>The contents of a note or notebook can be updated once it is created! Clicking on a note from an index will </p>", notebook_id: 1) # id: 5
+Note.create!(title: "Deleting", body: "<p>Notes, notebooks, and tags can also be deleted. When updating a note or notebook or viewing each tag of the Tags index, note that there exists a Trash icon for deleting the item. Be careful when deleting a notebook; it'll delete all the notes in that notebook as well!</p>", notebook_id: 1) # id: 6
 Note.create!(title: "Have fun exploring!", body: "<p>Create a new notebook or use one of the pre-created ones to start writing notes!</p>", notebook_id: 1) # id: 7
-Tag.create!(name: "welcome") # id: 1
-Tag.create!(name: "notebinder") # id: 2
+Tag.create!(name: "notebinder") # id: 1
+Tag.create!(name: "welcome") # id: 2
 Tag.create!(name: "introduction") # id: 3
 Tag.create!(name: "notes") # id: 4
 Tag.create!(name: "notebooks") # id: 5
@@ -28,6 +28,12 @@ Tag.create!(name: "tags") # id: 6
 Tagging.create!(note_id: 1, tag_id: 1)
 Tagging.create!(note_id: 1, tag_id: 2)
 Tagging.create!(note_id: 1, tag_id: 3)
+Tagging.create!(note_id: 2, tag_id: 1)
 Tagging.create!(note_id: 2, tag_id: 4)
+Tagging.create!(note_id: 3, tag_id: 1)
 Tagging.create!(note_id: 3, tag_id: 5)
+Tagging.create!(note_id: 4, tag_id: 1)
 Tagging.create!(note_id: 4, tag_id: 6)
+Tagging.create!(note_id: 5, tag_id: 1)
+Tagging.create!(note_id: 6, tag_id: 1)
+Tagging.create!(note_id: 7, tag_id: 1)
