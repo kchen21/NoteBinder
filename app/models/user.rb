8727734f3323e23e71617bd56e37044986972388
Kenneth Chen <kennethchendbgt@gmail.com>
2016-12-16 02:44:15 -0500

    Edit production environment configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
 
   attr_reader :password
 
-  has_attached_file :avatar, default_url: "default_avatar.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "default_avatar.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   has_many :notebooks,
