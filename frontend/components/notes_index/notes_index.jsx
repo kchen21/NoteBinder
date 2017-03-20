340bedda8a8f3ca4288e4c1ca0e644720bae609d
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-20 17:56:46 -0400

    Update CSS for New Note and Update Note pages

--- frontend/components/notes_index/notes_index.jsx
+++ frontend/components/notes_index/notes_index.jsx
@@ -51,7 +51,7 @@ class NotesIndex extends React.Component {
     }
 
     return (
-      <div className="group">
+      <div>
         <section className="notes-index">
           <section className="notes-index-header">
             <h1>NOTES</h1>
